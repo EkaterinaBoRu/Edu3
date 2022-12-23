aa079d5a27704e79506af2103e7114b838e06d90
ekaterina <rudkate21@mail.ru>
2022-12-23 21:04:05 +0300

    create zadacha 19

---
+++ Zadacha19/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+int GetUserValue(string message)
+{
+    Console.WriteLine (message);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+string CheckPolindrom (int n)
+{
+    if (n%100 == (n/10000+ ((n/1000)%10)*10))
+    {
+      return "Число является полиндромом";  
+    }
+    else return "Число не является полиндромом";
+}   
+
+
+int number = GetUserValue("Введите пятизначное число");
+string result = CheckPolindrom (number);
+
+Console.WriteLine($"Вывод: {result}");
