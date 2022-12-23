ce7e5179af68112dc61a80dcfa1e308a6e996ce1
ekaterina <rudkate21@mail.ru>
2022-12-23 21:37:14 +0300

    create zadacha 23

---
+++ Zadacha23/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int GetUserValue(string message)
+{
+    Console.WriteLine (message);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+void GetKvadrat(int n)
+{
+    if (n > 1)
+    {
+        for (int i=1; i<= n; i++)
+    {
+        int temp = i*i*i;
+        Console.Write($" {temp},");
+    }
+    }
+    else Console.WriteLine("Число ненатуральное");
+}
+
+int N = GetUserValue("Введите число N");
+GetKvadrat(N);
